8e6eeeabe74debffc63aff427692ff779a7bb6e4
Andrey Vitsentovich <vitsencool@gmail.com>
2019-12-16 20:22:14 +0200

    Add logout component.

---
+++ resources/js/components/auth/LogoutComponent.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script>
+    import {AUTH_LOGOUT} from '../../store/actions/auth';
+
+    export default {
+        name: "LogoutComponent",
+        created() {
+            this.$store.dispatch(AUTH_LOGOUT).then(() => this.$router.push({name: 'Login'}));
+        }
+    }
+</script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './components/HomeComponent'
 import Account from './components/AccountComponent'
 import Login from './components/auth/LoginComponent'
+import Logout from './components/auth/LogoutComponent';
 import store from './store/storage'
 
 Vue.use(Router);
@@ -10,16 +11,20 @@ Vue.use(Router);
 const ifNotAuthenticated = (to, from, next) => {
     if (!store.getters.isAuthenticated) {
         next();
+
         return
     }
+
     next('/')
 };
 
 const ifAuthenticated = (to, from, next) => {
     if (store.getters.isAuthenticated) {
         next();
+
         return
     }
+
     next('/login')
 };
 
@@ -30,6 +35,7 @@ export default new Router({
             path: '/',
             name: 'Home',
             component: Home,
+            beforeEnter: ifAuthenticated
         },
         {
             path: '/account',
@@ -42,6 +48,12 @@ export default new Router({
             name: 'Login',
             component: Login,
             beforeEnter: ifNotAuthenticated,
+        },
+        {
+            path: '/logout',
+            name: 'Logout',
+            component: Logout,
+            beforeEnter: ifAuthenticated,
         }
     ]
 })
